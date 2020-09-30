73544e6f1145ab4a4f52e8192184c17a9f8d7cb2
Elvix Marrugo <elvismarrugo@gmail.com>
2020-09-29 20:58:46 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const fibonacci = (n) => {
 
+  let arrayFibonacci = [1, 1];
+
+  for (let i = 0; i < n; i++) {
+    let count = arrayFibonacci[i] + arrayFibonacci[i + 1];
+    arrayFibonacci.push(count);
   }
+  arrayFibonacci.length = n;
+  return arrayFibonacci;
+}
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
