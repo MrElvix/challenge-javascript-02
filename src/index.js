4e60c47ce338602f6dfe9346061a1eb685c24970
MrElvix <elvismarrugo@gmail.com>
2020-10-02 17:17:13 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const fibonacci = (n) => {
-debugger;
+
   const arrayFibonacci = [0, 1];
 
   for (let i = 2; i <= n; i++) {
-    arrayFibonacci[i] = arrayFibonacci[i - 2] + arrayFibonacci[i - 1];
-    // let count = arrayFibonacci[i - 2] + arrayFibonacci[i - 1];
-    // arrayFibonacci.shift(count);
+    arrayFibonacci[i] = arrayFibonacci[i - 2] + arrayFibonacci[i - 1];   
   }
-  // arrayFibonacci.length = n;
+  
   arrayFibonacci.shift(); /*Eliminar el primer elemento de un Array*/
   return arrayFibonacci;
 }
